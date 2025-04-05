c14f90c63c1a434ac1fb91f517325b39807747bc
yeonda <yeonda@pusan.ac.kr>
2025-04-06 04:07:57 +0900

    modify mynumber.c to print odd and even numbers 1 to 100

--- mynumber.c
+++ mynumber.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main() {
+	printf("Odd numbers 1 to 100: \n");
 	for (int i = 1; i <= 100; i++) {
 		if (i % 2 != 0) printf("%d\n", i);
+	}
+	
+	printf("Even numbers 1 to 100: \n");
+	for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 0) printf("%d\n", i);
 	}
 	return 0;
 }
